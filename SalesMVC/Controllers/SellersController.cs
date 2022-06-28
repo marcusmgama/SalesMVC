041773fcfda8996844736d21f43d6602ea7f6231
Marcus Gama <marcusmgama@gmail.com>
2022-06-28 09:37:42 +0100

    Simple create form

--- SalesMVC/Controllers/SellersController.cs
+++ SalesMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesMVC.Models;
 using SalesMVC.Services;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,19 @@ namespace SalesMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- SalesMVC/Services/SellerService.cs
+++ SalesMVC/Services/SellerService.cs
@@ -20,5 +20,11 @@ namespace SalesMVC.Services
             return _context.Seller.ToList();
 
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
